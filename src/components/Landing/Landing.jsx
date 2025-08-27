81665ef8858df002607ef7ab7d017111f1069dac
pericarney00 <pericarney@gmail.com>
2025-08-27 11:44:44 -0500

    Changed source of bleepo images

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,20 +2,20 @@
 
 const Landing = () => {
 	return (
-		<>
-			<main className="container">
-				<h1>Welcome to Bleepr</h1>
-				<img
-					width={512}
-					src="src/assets/images/Bleepo-transparent.png"
-					alt="bleepo"
-				/>
-				<h2>What the !@#$ is Bleepr</h2>
-				<p>A retro social media site with no algorithms</p>
-				<p>Sign up now, or sign in to bleep around and find out!</p>
-			</main>
-		</>
-	);
+    <>
+      <main className="container">
+        <h1>Welcome to Bleepr</h1>
+        <img
+          width={512}
+          src="https://images.pexels.com/photos/33642104/pexels-photo-33642104.png"
+          alt="bleepo"
+        />
+        <h2>What the !@#$ is Bleepr</h2>
+        <p>A retro social media site with no algorithms</p>
+        <p>Sign up now, or sign in to bleep around and find out!</p>
+      </main>
+    </>
+  );
 };
 
 export default Landing;
