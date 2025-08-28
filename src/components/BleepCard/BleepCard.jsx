7987347e66e31a722b34031d745d979b88db9327
raworiginal <randall.a.wolfe@gmail.com>
2025-08-27 18:54:41 -0700

    almost done with bleepcard

--- src/components/BleepCard/BleepCard.jsx
+++ src/components/BleepCard/BleepCard.jsx
@@ -9,45 +9,52 @@ const BleepCard = (props) => {
 
 	return (
 		<article className={styles.bleepCard}>
-			<div className={styles.username}>
-				<p>{`@${props.bleep.author.username}`}</p>
-			</div>
-			<img
-				className={styles.avatar}
-				src={
-					props.bleep.author.profilePicture ||
-					`https://i.pravatar.cc/300?u=${props.bleep.author._id}`
-				}
-				alt="profile picture"
-			/>
-			<div className={styles.bleepText}>
-				<Link to={`/bleeps/${props.bleep._id}`}>
-					<p className="{styles.bleepText}">{props.bleep.text}</p>
-				</Link>
+			<div className={styles.leftColumn}>
+				<div className={styles.username}>
+					<span>{`@${props.bleep.author.username}`}</span>
+				</div>
+				<div className={styles.avatarFrame}>
+					<img
+						className={styles.avatar}
+						src={
+							props.bleep.author.profilePicture ||
+							`https://i.pravatar.cc/300?u=${props.bleep.author._id}`
+						}
+						alt="profile picture"
+					/>
+				</div>
+
+				<div className={styles.metrics} aria-label="engagement">
+					<span>❤️ 8</span>
+					<span>🔁 100</span>
+					<span>💬 4</span>
+				</div>
 			</div>
 
-			<div className={styles.metrics} aria-label="engagement">
-				<p>❤️ 8</p>
-				<p>🔁 100</p>
-				<p>💬 4</p>
-			</div>
+			<div className={styles.rightColumn}>
+				<div className={styles.post}>
+					<small>{`posted on ${new Date(
+						props.bleep.createdAt
+					).toLocaleDateString()}`}</small>
+				</div>
 
-			<div className={styles.post}>
-				<p>{`posted on ${new Date(
-					props.bleep.createdAt
-				).toLocaleDateString()}`}</p>
-			</div>
+				<article className={styles.bleepText}>
+					<Link to={`/bleeps/${props.bleep._id}`}>
+						<p className="{styles.bleepText}">{props.bleep.text}</p>
+					</Link>
+				</article>
 
-			<div className={styles.buttons}>
-				<a onClick={() => props.handleUpdateLike(props.bleep._id)}>like</a>
-				{props.bleep.author._id === bleepr._id && (
-					<>
-						<Link to={`/bleeps/${props.bleep._id}/edit`}>edit</Link>
-						<a onClick={() => props.handleDeleteBleep(props.bleep._id)}>
-							delete
-						</a>
-					</>
-				)}
+				<div className={styles.buttons}>
+					<a onClick={() => props.handleUpdateLike(props.bleep._id)}>like</a>
+					{props.bleep.author._id === bleepr._id && (
+						<>
+							<Link to={`/bleeps/${props.bleep._id}/edit`}>edit</Link>
+							<a onClick={() => props.handleDeleteBleep(props.bleep._id)}>
+								delete
+							</a>
+						</>
+					)}
+				</div>
 			</div>
 		</article>
 	);
