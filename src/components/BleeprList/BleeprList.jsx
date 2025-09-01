90c748d21eddde78bc5ba205e930492beedd7dcb
Tia Piol <tiarox02@yahoo.com>
2025-09-01 16:14:37 -0700

    updated styling for the find cool bleeprs

--- src/components/BleeprList/BleeprList.jsx
+++ src/components/BleeprList/BleeprList.jsx
@@ -1,41 +1,47 @@
 import { useContext } from "react";
 import { BleeprContext } from "../../contexts/BleeprContext";
+import styles from "./BleeprList.module.css"
 
 const BleeprList = (props) => {
 	const { bleepr } = useContext(BleeprContext);
 	return (
-		<>
-			<h1>find cool bleeprs</h1>
+<>
+  <h1 className={styles.title}>★ find cool bleeprs ★</h1>  {/* moved outside */}
 
-			{
-				props.bleeprs.length ? (
-					props.bleeprs.map((b) => (
-						<div key={b._id}>
-							<img src={b.profilePicture || `https://i.pravatar.cc/300?u=${b._id}`}
-								alt={b.username}
-							/>
-							<p>{b.username}</p>
-							
-							{
-								b.friends.includes(bleepr._id) ? (
-									<button onClick={() => props.handleUpdateFriends(b._id)}>
-										remove
-									</button>
-								) : (
-									<button onClick={() => props.handleUpdateFriends(b._id)}>
-										add
-									</button>
-								)
-							}
-						
-						</div>
-					))
-				) : (
-					<p>no Bleeprs yet</p>
-				)
-			}
+  <div className={styles.bleeprContainer}>
+    {props.bleeprs.length ? (
+      props.bleeprs.map((b) => (
+        <div key={b._id} className={styles.bleeprCard}>
+          <img
+            className={styles.bleeprPic}
+            src={b.profilePicture || `https://i.pravatar.cc/300?u=${b._id}`}
+            alt={b.username}
+          />
+          <p className={styles.bleeprName}>{b.username}</p>
 
-		</>
+          {b.friends.includes(bleepr._id) ? (
+            <button
+              className={`${styles.bleeprBtn} ${styles.remove}`}
+              onClick={() => props.handleUpdateFriends(b._id)}
+            >
+              remove
+            </button>
+          ) : (
+            <button
+              className={`${styles.bleeprBtn} ${styles.add}`}
+              onClick={() => props.handleUpdateFriends(b._id)}
+            >
+              add
+            </button>
+          )}
+        </div>
+      ))
+    ) : (
+      <p className={styles.noBleeprs}>no Bleeprs yet :(</p>
+    )}
+  </div>
+</>
+
 	);
 };
 
